4df195a845a641093ebc45d37e8cec3041ad7998
James Gardner <jamesgardner@live.com.au>
2019-06-17 12:02:23 +1000

    16.3.2

--- src/scanner.c
+++ src/scanner.c
@@ -31,6 +31,11 @@ static char peek() {
 	return *scanner.current;
 }
 
+static char peekNext() {
+	if (isAtEnd()) return '\0';
+	return scanner.current[1];
+}
+
 static bool match(char expected) {
 	if (isAtEnd()) return false;
 	if (*scanner.current != expected) return false;
@@ -72,6 +77,15 @@ static void skipWhitespace() {
 			case '\n':
 				scanner.line++;
 				advance();
+				break;
+
+			case '/':
+				if (peekNext() == '/') {
+					// A comment goes until the end of the line.
+					while (peek() != '\n' && !isAtEnd()) advance();
+				} else {
+					return;
+				}
 				break;
 
 			default:
