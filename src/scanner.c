f4336594bd7eec8080f5ce8df68851b0ecc0d9e6
James Gardner <jamesgardner@live.com.au>
2019-06-17 12:06:19 +1000

    16.3

--- src/scanner.c
+++ src/scanner.c
@@ -18,6 +18,10 @@ void initScanner(const char* source) {
 	scanner.line = 1;
 }
 
+static bool isDigit(char c) {
+	return c >= '0' && c <= '9';
+}
+
 static bool isAtEnd() {
 	return *scanner.current == '\0';
 }
@@ -94,6 +98,33 @@ static void skipWhitespace() {
 	}
 }
 
+static Token number() {
+	while (isDigit(peek())) advance();
+
+	// Look for a fractional part.
+	if (peek() == '.' && isDigit(peekNext())) {
+		// Consume the "."
+		advance();
+
+		while (isDigit(peek())) advance();
+	}
+
+	return makeToken(TOKEN_NUMBER);
+}
+
+static Token string() {
+	while (peek() != '"' && !isAtEnd()) {
+		if (peek() == '\n') scanner.line++;
+		advance();
+	}
+
+	if (isAtEnd()) return errorToken("Unterminated string.");
+
+	// The closing ".
+	advance();
+	return makeToken(TOKEN_STRING);
+}
+
 Token scanToken() {
 	skipWhitespace();
 
@@ -102,6 +133,7 @@ Token scanToken() {
 	if (isAtEnd()) return makeToken(TOKEN_EOF);
 
 	char c = advance();
+	if (isDigit(c)) return number();
 
 	switch (c) {
 		case '(': return makeToken(TOKEN_LEFT_PAREN);
@@ -123,6 +155,8 @@ Token scanToken() {
 			return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
 		case '>':
 			return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+
+		case '"': return string();
 	}
 
 	return errorToken("Unexpected character.");
