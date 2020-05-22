005a6cfa32db23b8a1df75dd7c77b47f56cf26ec
James Gardner <jamesgardner@live.com.au>
2020-05-22 17:17:19 +1000

    26.5 When to collect

--- src/memory.c
+++ src/memory.c
@@ -10,10 +10,18 @@
 #include "debug.h"
 #endif
 
+#define GC_HEAP_GROWTH_FACTOR 2
+
 void* reallocate(void* previous, size_t oldSize, size_t newSize) {
+	vm.bytesAllocated += newSize - oldSize;
+
 	if (newSize > oldSize) {
 #ifdef DEBUG_STRESS_GC
 		collectGarbage();
+#else
+		if (vm.bytesAllocated > vm.nextGC) {
+			collectGarbage();
+		}
 #endif
 	}
 
@@ -175,6 +183,7 @@ static void sweep() {
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
 	printf("-- gc begin\n");
+	size_t before = vm.bytesAllocated;
 #endif
 
 	markRoots();
@@ -182,8 +191,13 @@ void collectGarbage() {
 	tableRemoveWhite(&vm.strings);
 	sweep();
 
+	vm.nextGC = vm.bytesAllocated * GC_HEAP_GROWTH_FACTOR;
+
 #ifdef DEBUG_LOG_GC
 	printf("-- gc end\n");
+	printf("   collected %ld bytes (from %ld to %ld) next at %ld\n",
+		   before - vm.bytesAllocated, before, vm.bytesAllocated,
+		   vm.nextGC);
 #endif
 }
 
