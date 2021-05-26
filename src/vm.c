e013396aa983bb8bbcef32ec47acf5b6b0430153
James Gardner <jamesgardner@live.com.au>
2021-05-26 15:57:27 +1000

    28.4 Instance initializers

--- src/vm.c
+++ src/vm.c
@@ -87,6 +87,9 @@ void initVM() {
 	initTable(&vm.globals);
 	initTable(&vm.strings);
 
+	vm.initString = NULL;
+	vm.initString = copyString("init", 4);
+
 	defineNative("clock", clockNative);
 	defineNative("isDefined", isDefinedNative);
 }
@@ -94,6 +97,7 @@ void initVM() {
 void freeVM() {
 	freeTable(&vm.globals);
 	freeTable(&vm.strings);
+	vm.initString = NULL;
 	freeObjects();
 }
 
@@ -142,6 +146,13 @@ static bool callValue(Value callee, int argCount) {
 			case OBJ_CLASS: {
 				ObjClass* klass = AS_CLASS(callee);
 				vm.stackTop[-argCount - 1] = OBJ_VAL(newInstance(klass));
+				Value initializer;
+				if (tableGet(&klass->methods, vm.initString, &initializer)) {
+					return call(AS_CLOSURE(initializer), argCount);
+				} else if (argCount != 0) {
+					runtimeError("Expected 0 arguments but got %d.", argCount);
+					return false;
+				}
 				return true;
 			}
 			case OBJ_CLOSURE:
